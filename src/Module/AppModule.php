d43b97b900d27c72d23431302b1aea428e156120
Akihito Koriyama <akihito.koriyama@gmail.com>
2023-04-19 16:35:20 +0900

    StorageRedisModuleをインストール

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -7,6 +7,7 @@ namespace MyVendor\MyProject\Module;
 use BEAR\Dotenv\Dotenv;
 use BEAR\Package\AbstractAppModule;
 use BEAR\Package\PackageModule;
+use BEAR\QueryRepository\StorageRedisModule;
 
 use function dirname;
 
@@ -15,6 +16,7 @@ class AppModule extends AbstractAppModule
     protected function configure(): void
     {
         (new Dotenv())->load(dirname(__DIR__, 2));
+        $this->install(new StorageRedisModule('127.0.0.1:6379'));
         $this->install(new PackageModule());
     }
 }
